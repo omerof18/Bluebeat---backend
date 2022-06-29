fceabf5c45ea971bf6d727ff09a68e3a934b9262
omerof18 <108413595+omerof18@users.noreply.github.com>
2022-06-29 17:25:59 +0300

    Add files via upload

---
+++ bluebeat-backend/config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  "dbURL": "mongodb+srv://bluebeat:123@cluster0.wnlut.mongodb.net/atlas_DB?retryWrites=true&w=majority"
+}
---
+++ bluebeat-backend/public/precache-manifest.c9289b68cb9ef5e3d067967e0231b5a4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caab4c8e2308e8f4b089",
+    "url": "/css/app.6aaed823.css"
+  },
+  {
+    "revision": "5de248d4cfa58f54dd9a",
+    "url": "/css/chunk-vendors.84bb20f7.css"
+  },
+  {
+    "revision": "a35344daceea252f94e2d8595d92e90f",
+    "url": "/fonts/Ambit-Bold.a35344da.otf"
+  },
+  {
+    "revision": "aa690cb50557003c3493e75a113a7f2d",
+    "url": "/fonts/Ambit-Light.aa690cb5.otf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "0ce30fabcdc50c336121274118517361",
+    "url": "/img/beatLogo.0ce30fab.png"
+  },
+  {
+    "revision": "d6d6d24128f208d00658d5de3e13da75",
+    "url": "/img/bluebeatHero.d6d6d241.jpg"
+  },
+  {
+    "revision": "898f0d174f0bb5325e99ea14fb68bd0b",
+    "url": "/img/default-guest.898f0d17.jpg"
+  },
+  {
+    "revision": "bd11a7c31ff26de0b7cbe714d7a4a132",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caab4c8e2308e8f4b089",
+    "url": "/js/app.03887e99.js"
+  },
+  {
+    "revision": "5de248d4cfa58f54dd9a",
+    "url": "/js/chunk-vendors.248ee8ec.js"
+  },
+  {
+    "revision": "1d6e61ec6626fe91ccc3fc27b2b76278",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
